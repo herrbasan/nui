29d3aca955b5d557a59cc0188c8fbfc785dbf3d3
herrbasan <david@raum.com>
2024-03-31 08:34:50 +0200

    Update nui_app.js
    DropZone Added

--- nui_app.js
+++ nui_app.js
@@ -1,15 +1,18 @@
 'use strict';
 import ut from './nui_ut.js';
 let css_vars;
-let appTools = {renderWindow:renderWindow};
+let appTools = {
+    appWindow:appWindow,
+    dropZone:dropZone
+};
 
 /* App Tools
 ##########################################################################################
 ########################################################################################## */
 
-async function renderWindow(prop){
+async function appWindow(prop){
     return new Promise(async (resolve, rejcet) => {
-        let html = renderHTML(prop);
+        let html = renderWindowFrame(prop);
         await checkCSS('./nui/css/nui_app.css', '--nui-app');
         ut.killKids(document.body);
         document.body.appendChild(html);
@@ -17,7 +20,7 @@ async function renderWindow(prop){
     })
 }
 
-function renderHTML(prop){
+function renderWindowFrame(prop){
     let title = document.title || 'Window'
     let inner = prop?.inner || '';
     if(ut.isElement(inner)){ inner = inner.innerHTML}
@@ -38,9 +41,16 @@ function renderHTML(prop){
         <div class="nui-status-bar"></div>
     </div>
     `)
+
     if(prop.fnc_close){
         html.el('.nui-title-bar .close').addEventListener('click', prop.fnc_close);
     }
+    else {
+        if(window.electron_helper){
+            html.el('.nui-title-bar .close').addEventListener('click', electron_helper.app.exit);
+        }
+    }
+
     if(window.electron_helper){
         window.electron_helper.window.hook_event('focus', winEvents);
 	    window.electron_helper.window.hook_event('blur', winEvents);
@@ -50,6 +60,68 @@ function renderHTML(prop){
         if(e.type == 'blur'){ ut.el('body').removeClass('focus');}
     }
 
+    return html;
+}
+
+async function dropZone(ar, fnc, target){
+    return new Promise(async (resolve, rejcet) => {
+        let html = renderDropZone(ar, fnc);
+        await checkCSS('./nui/css/nui_app.css', '--nui-app');
+        if(target){ target.appendChild(html)}
+        resolve(html);
+    })
+}
+
+function renderDropZone(ar, fnc){
+    let id = 'dropzone_' + ut.id();
+    if(!fnc) { fnc = console.log }
+    let html = ut.htmlObject(/*html*/`
+        <div id="${id}" class="nui-dropzone">
+            <div class="nui-dropzone-content"></div>
+        </div>
+    `)
+    html.content = html.el('.nui-dropzone-content');
+    for(let i=0; i<ar.length; i++){
+        let item = ut.createElement('div', {id:ar[i].name, class:'item', inner:ar[i].label, target:html.content});
+        item.addEventListener('dragleave', dropzoneHover);
+        item.addEventListener('dragenter', dropzoneHover);
+    }
+    window.dropzone = id;
+    window.addEventListener('dragover', (e) => { e.preventDefault(); })
+	window.addEventListener('dragleave', showDropzone)
+	window.addEventListener('dragenter', showDropzone)
+	window.addEventListener('drop', showDropzone)
+
+    function dropzoneHover(e){
+        if(e.type == 'dragenter'){
+            e.currentTarget.addClass('active');
+        }
+        if(e.type == 'dragleave'){
+            e.currentTarget.removeClass('active');
+        }
+    }
+
+    async function showDropzone(e){
+        if(e.type == 'dragenter'){
+            html.style.display = 'block';
+            html.content.style.opacity = 1;
+        }
+        if(e.type == 'dragleave' && e.target.id == id){
+            html.style.display = null;
+            html.content.style.opacity = 0;
+        }
+        if(e.type == 'drop'){
+            html.style.display = null;
+            html.content.style.opacity = 0;
+            let items = html.content.els('.item');
+            for(let i=0; i<items.length; i++){
+                items[i].removeClass('active');
+            }
+            e.preventDefault();
+            
+            fnc(e.dataTransfer.files);
+        }
+    }
     return html;
 }
 
